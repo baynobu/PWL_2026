aef455b06cd21dd5051cbfd19f402317bcaed6c3
baynobu <darmawanhanif30@gmail.com>
2026-02-26 10:16:35 +0700

    Praktikum 2

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\PhotoController;
 
-Route::get('/hello', function () {
-    return 'Hello World';
-});
+Route::resource('photos', PhotoController::class);
+
+Route::resource('photos', PhotoController::class)->only([
+    'index', 'show'
+]);
+
+Route::resource('photos', PhotoController::class)->except([
+    'create', 'store', 'update', 'destroy'
+]);
+
+Route::get('/ hello', [WelcomeController::class, 'hello']);
 
 Route::get('/world', function () {
     return 'World';
@@ -17,11 +27,9 @@ Route::get('/user/{name}', function ($name) {
 Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
     return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
 });
-
 Route::get('/user/{name?}', function ($name = null) {
     return 'Abdul Rahman Hanif Darmawan ' . $name;
 });
-
 Route::get('/user/{name?}', function ($name = 'John') {
     return 'Abdul Rahman Hanif Darmawan ' . $name;
 });
