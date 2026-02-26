e8c4248ab54d19bc2e05fc8025ef413dfa81923e
baynobu <darmawanhanif30@gmail.com>
2026-02-26 09:59:35 +0700

    Praktikum 1

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,26 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/user/{name}', function ($name) {
+    return 'Abdul Rahman Hanif Darmawan ' . $name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
+});
+
+Route::get('/user/{name?}', function ($name = null) {
+    return 'Abdul Rahman Hanif Darmawan ' . $name;
+});
+
+Route::get('/user/{name?}', function ($name = 'John') {
+    return 'Abdul Rahman Hanif Darmawan ' . $name;
 });
